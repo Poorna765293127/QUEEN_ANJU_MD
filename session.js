1137d99582bf758dfde9c6f7707fce99e96482ff
Poorna765293127 <Janiduchamath725@gmail.com>
2024-12-14 20:02:30 +0530

    Update session.js

--- session.js
+++ session.js
@@ -52,5 +52,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=zM0mHYwA#s7ivtfVCoNepXwhti2aOLyTodprHCiqhHhdVfNa_FCY",
 };
